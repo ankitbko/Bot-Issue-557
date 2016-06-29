bf3a53bf6c0a103203ce5041b757e2e9121f744d
'Ankit <ankitbko@gmail.com>
2016-06-29 12:14:06 +0530

    Added core project

---
+++ SampleBotCore/Controllers/MessagesController.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleBot
+{
+    [BotAuthentication("", "")]
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        internal static IDialog<Employee> MakeRoot()
+        {
+            return Chain.From(() => new SampleDialog()).DefaultIfException();
+        }
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [HttpPost]
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                return await Conversation.SendAsync(message, MakeRoot);
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+            }
+        }
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+    }
+}
